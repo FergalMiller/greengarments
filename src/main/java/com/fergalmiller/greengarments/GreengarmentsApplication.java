cc46f87379d6df086f3a2c51418c39e44fb2b5f1
FergalMiller <fergalmiller@hotmail.co.uk>
2019-10-09 18:49:19 +0100

    Initial commit

---
+++ src/main/java/com/fergalmiller/greengarments/GreengarmentsApplication.java
@@ -0,0 +1,13 @@
+package com.fergalmiller.greengarments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreengarmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreengarmentsApplication.class, args);
+	}
+
+}
